3f820d4536c7908845aaef88cf3bbda943e97c22
Bego96 <begic.ismar96@gmail.com>
2024-02-26 19:51:21 +0100

    Fixed gallery on smaller screens

--- src/components/imgGallery.js
+++ src/components/imgGallery.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Image from 'next/image';
 
-export default function ImgGallery({ gallery }) {
+export default function ImgGallery({ gallery, key, imgSrc }) {
   return (
     <>
-      {gallery.map((img, index) => (
-        <div key={img.id} className='w-full mb-4 md:h-[360px] md:w-auto md:mx-auto md:mb-20'>
-          <Image src={img.imgSrc} className="h-full w-full" width={500} height={1000} alt="dog" />
-        </div>
-      ))}
+      {
+      
+        window.innerWidth > 1023 ?
+        gallery.map((img, index) => (
+          <div key={img.id} className='w-full mb-4 md:h-[360px] md:w-auto md:mx-auto md:mb-20'>
+            <Image src={img.imgSrc} className="h-full w-full" width={500} height={1000} alt="dog" />
+          </div>
+      )) : <div key={key} className='h-full flex justify-center'>
+            <Image src={imgSrc} className="min-h-[300px] w-auto" width={500} height={1000} alt="dog" />
+          </div>
+          }
     </>
   );
 }

c7926ab6f3d7de8435beb8279e51b57a0ec5e94d
Bego96 <begic.ismar96@gmail.com>
2024-02-26 20:13:56 +0100

    Attempted fix

--- src/components/dogPhotos.js
+++ src/components/dogPhotos.js
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import  { useState } from 'react';
+import  { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import ImgGallery from './imgGallery';
